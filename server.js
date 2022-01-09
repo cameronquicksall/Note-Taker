d27d96fda2f6ec1f24f521bfeedddd1f74885eb7
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-01-09 14:50:27 -0600

    route for notes

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 // Required dependencies
 const express = require("express");
+const { join } = require("path");
 const path = require("path");
 
 // Set port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Adds listener
 app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
 });
 
-//Route for index file
+// Route for index file
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Route for notes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes/html"));
 });
 
 
