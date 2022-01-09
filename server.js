187aeb37a2d7b7a0fef14d5b0abe89a41aa988fa
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-01-09 14:41:18 -0600

    added route for index

--- server.js
+++ server.js
@@ -2,18 +2,19 @@
 const express = require("express");
 const path = require("path");
 
-// Sets port 
+// Set port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Adds listener
 app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
 });
 
-// Route for index file
+//Route for index file
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
+
+
 
 
