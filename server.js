291f074d332ee5ad1a13aa2c0200012b5ae0ba05
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-01-09 15:47:20 -0600

    added routes for notes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Required dependencies
 const express = require("express");
-const { join } = require("path");
 const path = require("path");
+const fs = require("fs");
 
-// Set port
+// Sets port 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,16 +12,21 @@ app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
 });
 
+// Route for notes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+})
+
 // Route for index file
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
-
-// Route for notes
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes/html"));
-});
-
-
-
-
