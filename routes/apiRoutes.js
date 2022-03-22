9c6f41f1d24764018550be71d2901fb66b3d16f6
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-03-22 16:16:56 -0500

    update post route to give new entry an id + implement fs to store collected data as string in db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const db = require('../db/db.json')
 const router = require('express').Router();
+const fs = require('fs');
 
 // GET route for notes
 router.get("/notes", function(req, res) {
@@ -8,15 +9,21 @@ router.get("/notes", function(req, res) {
 
 // POST route for notes
 router.post("/notes", (req, res) => {
-    db.push(req.body);
-    res.json(req.body);
-    console.log(`Note ${db} has been saved!`);
+    let newNote = req.body;
+    // if first entry, give it an id starting at index of 0
+    if (db.length === 0) {
+        newNote.id = '0';
+    } else {
+    // else, increment id by 1 per entry
+        newNote.id = JSON.stringify(JSON.parse(db[db.length - 1].id) +1 );
+    }
+    // push method to add the new note to the end of our array
+    db.push(newNote);
+    // store collected data from our object as a string and pass it into db.json using fs module
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
+    res.json(newNote);
+    console.log(`New note ${newNote} has been saved!`);
 });
-
-//Delete method
-// router.delete("/notes/:id", (req, res) => {
-
-// });
 
 
 module.exports = router;
